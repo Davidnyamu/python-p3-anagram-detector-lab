bd3cbf0f776047724ea391a32c76c7eb5aad1e42
+ <+>
2024-05-29 14:51:00 +0300

    finialcommit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+    
+    def match(self, words):
+        return [d for d in words if sorted(d) == self.sorted_word]
